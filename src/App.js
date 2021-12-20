d09f858972e316c8737b8844fa5e073767e795af
Emeka Victor Mbachu <victor.c.mbachu@gmail.com>
2021-12-20 21:23:03 +0100

    lightening fast page load using local state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import list from './list';
 import React, {Component} from "react";
+import { Container, Row, FormGroup} from 'react-bootstrap';
+
+// Default parameters to fetch data from API
+const DEFAULT_QUERY = 'react';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = 100;
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+// const url = PATH_BASE + PATH_SEARCH + '?' + PARAM_SEARCH + DEFAULT_QUERY;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}${DEFAULT_HPP}`;
+console.log(url);
 
 // Filter the results by search
 function isSearched(searchTerm){
@@ -18,20 +34,69 @@ class App extends Component{
     // super props sets this.props to the constructor
     super(props);
 
+    // Setting up state
     this.state = {
-      list: list,
-      searchTerm: ''
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY
     }
 
+    // bind the functions to this (app component)
     this.removeItem = this.removeItem.bind(this);
     this.searchvalue = this.searchValue.bind(this);
+    this.fetchTopStories = this.fetchTopStories.bind(this);
+    this.setTopStories = this.setTopStories.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
   }
 
-  // let's rewrite removeItem function in ES6
+  // Set top stories
+    setTopStories(result){
+      // get hits and page from result
+      const {hits, page} = result;
+      // meaning page is not 0, button might has been clicked, page might be 1 or 2
+        // Old hits are already available in the state
+      // const oldHits = page !== 0 ? this.state.result.hits : [];
+        const { searchKey, results } = this.state;
+        const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+      const updatedHits = [...oldHits, ...hits];
+      this.setState({ results: { ...results, [searchKey]: {hits: updatedHits, page} } });
+    }
+
+  // Fetch the stories
+  fetchTopStories(searchTerm, page){
+      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+          .then(response => response.json())
+          .then(result => this.setTopStories(result))
+          .catch( e => e);
+  }
+
+    // component did mount
+    componentDidMount() {
+      const { searchTerm } = this.state;
+      this.setState({ searchKey: searchTerm });
+      this.fetchTopStories(searchTerm, DEFAULT_PAGE);
+    }
+
+    // on search submit function
+    onSubmit(event){
+      const { searchTerm } = this.state;
+      this.setState({ searchKey: searchTerm });
+      this.fetchTopStories(searchTerm, DEFAULT_PAGE);
+      event.preventDefault();
+    }
+
+    // let's rewrite removeItem function in ES6
   removeItem(id){
-    const isNotId = item => item.objectId !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const { results, searchKey } = this.state;
+    const {hits, page} = results[searchKey];
+    // const isNotId = item => item.objectId !== id;
+    const updatedList = hits.filter(item => item.objectID !== id);
+    // Object assign
+    // this.setState({ result: Object.assign({}, this.state.result, {hits: updatedList}) });
+
+    // Spread operator
+    this.setState({ results: {...results, [searchKey]: {hits: updatedList, page}} });
   }
 
   // Get input fields value from search form
@@ -42,23 +107,43 @@ class App extends Component{
 
   render (){
 
-    const { list, searchTerm } = this.state;
+    const { results, searchTerm, searchKey } = this.state;
+
+    // if(!result){ return null; }
+
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
 
     console.log(this);
 
     return (
-        <Search className="App">
+        <div>
 
-          <Search
-              onChange={ this.searchValue }
-              value={ searchTerm }
-          >Search Here</Search>
+          <Container fluid>
+              <Row>
+                  <div className="jumbotron text-center">
+                  <Search
+                      onChange={ this.searchValue }
+                      value={ searchTerm }
+                      onSubmit={ this.onSubmit }
+                  >News App</Search>
+                  </div>
+              </Row>
+          </Container>
 
-          <Table
+        <Table
             list={ list }
             searchTerm={ searchTerm }
             removeItem={ this.removeItem }
-          />
+        />
+
+        <div className="text-center alert">
+            <button
+                className="btn btn-success"
+                onClick={ () => this.fetchTopStories(searchTerm, page + 1) }>
+                Load more
+            </button>
+        </div>
 
         </div>
     );
@@ -66,47 +151,69 @@ class App extends Component{
   }
 }
 
-class Search extends Component {
-  render() {
-    const { onChange, value, children } = this.props;
-    return(
-      <form>
-        { children }
-        <input
-            type="text"
-            onChange={ this.props.onChange }
-            value={ this.props.value }
-        />
+// class Search extends Component {
+//   render() {
+//     const { onChange, value, children } = this.props;
+//   }
+// }
+
+const Search = ({ onChange, value, children, onSubmit }) => {
+  return(
+      <form onSubmit={ onSubmit }>
+          <FormGroup>
+          <h1 style={{ fontWeight: 'bold' }}>{ children }</h1>
+          <hr style={{ border: '2px solid black', width: '100px' }} />
+          <div className="input-group">
+              <input
+                  className="form-control width100 searchForm"
+                  type="text"
+                  onChange={ onChange }
+                  value={ value }
+              />
+
+              <span className="input-group-btn">
+                <Button
+                    className="btn btn-primary searchBtn"
+                    type="submit">Search
+                </Button>
+              </span>
+
+          </div>
+          </FormGroup>
       </form>
-    )
-  }
+  )
 }
 
-class Table extends Component {
-  render(){
-    const { list, searchTerm, removeItem} = this.props;
-    return(
-        <div>
-          {
-            list.filter( isSearched(searchTerm) ).map((item) => {
+const Table = ({ list, searchTerm, removeItem}) => {
+  return(
+      <div className="col-sm-10 col-sm-offset-1">
+        {
+          list.map((item) => {
 
-              return (<div key={item.objectId}>
-                <h1>
-                  <a href={ item.url }>{item.title}</a> by {item.author}
-                </h1>
-                <h4>
-                  {item.num_comments} comments | { item.points } points
-                </h4>
+            return (<div key={item.objectId}>
+              <h1>
+                <a href={ item.url }>{item.title}</a> by {item.author}
+              </h1>
+              <h4>
+                {item.num_comments} Comments | { item.points } Points
+                  <button className="btn btn-danger btn-xs"
+                          type="button"
+                          onClick={() => removeItem(item.objectId)}>
+                      Remove</button>
+              </h4><hr />
 
-                <button type="button"
-                        onClick={() => removeItem(item.objectId)}>
-                  Remove</button>
-              </div>);
-            })
-          }
-        </div>
-    )
-  }
+            </div>);
+          })
+        }
+      </div>
+  )
 }
+
+const Button = ({ onclick, children, className='' }) =>
+    <button
+        className={ className }
+        onClick={ onclick }>
+      { children }
+    </button>
 
 export default App;
